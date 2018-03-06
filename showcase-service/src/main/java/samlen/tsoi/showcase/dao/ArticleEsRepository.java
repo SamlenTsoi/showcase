5897615ce6264ee3059db8555b8525e5f5340c7c
Samlen_Tsoi <samlen_tsoi@Samlen-TsoideMacBook-Pro.local>
2018-03-06 11:20:11 +0800

    es微调

--- showcase-service/src/main/java/samlen/tsoi/showcase/dao/ArticleEsRepository.java
+++ showcase-service/src/main/java/samlen/tsoi/showcase/dao/ArticleEsRepository.java
@@ -1,13 +1,11 @@
 package samlen.tsoi.showcase.dao;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 import samlen.tsoi.showcase.entity.ArticleEs;
 
 /**
  * @author samlen_tsoi
  * @date 2018/1/18
  */
-@Repository
 public interface ArticleEsRepository extends ElasticsearchRepository<ArticleEs, Long> {
 }

1fb728771876dd24f3b89a5a9cae92d14ceecc9a
Samlen_Tsoi <samlentsoi@icloud.com>
2019-11-14 15:30:05 +0800

    增加zuul filer.

---
+++ showcase-cloud/showcase-cloud-zuul/src/main/java/samlen/tsoi/showcase/cloud/zuul/filter/SampleFilter.java
@@ -0,0 +1,61 @@
+package samlen.tsoi.showcase.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 过滤器
+ *
+ * @author samlen_tsoi
+ * @date 2019/11/14
+ **/
+@Slf4j
+@Component
+public class SampleFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info("--->>> TokenFilter {}, {}", request.getMethod(), request.getRequestURL().toString());
+
+        // 获取请求的参数
+        String token = request.getParameter("token");
+
+        if (StringUtils.isNotBlank(token)) {
+            // 对请求进行路由
+            ctx.setSendZuulResponse(true);
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            // 不对其进行路由
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+    }
+}

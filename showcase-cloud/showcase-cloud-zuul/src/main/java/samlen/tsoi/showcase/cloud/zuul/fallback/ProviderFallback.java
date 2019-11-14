9ae81078df678fea08926f0b7d129dedf5be71c3
Samlen_Tsoi <samlentsoi@icloud.com>
2019-11-14 15:43:14 +0800

    增加zuul fallback。

---
+++ showcase-cloud/showcase-cloud-zuul/src/main/java/samlen/tsoi/showcase/cloud/zuul/fallback/ProviderFallback.java
@@ -0,0 +1,71 @@
+package samlen.tsoi.showcase.cloud.zuul.fallback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author samlen_tsoi
+ * @date 2019/11/14
+ **/
+@Slf4j
+@Component
+public class ProviderFallback implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "showcase-provider";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+            log.info("Exception {}",reason);
+        }
+        return fallbackResponse();
+    }
+
+    private ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
